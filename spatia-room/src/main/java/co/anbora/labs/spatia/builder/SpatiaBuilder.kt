acccf7217ab197d733036c805902fb2e283e76de
Darwin Algarin <dalgarins@live.com>
2020-05-05 23:52:58 -0500

    Limited builder scope to avoid calls to methods like:
      - createFromAsset
      - createFromFile

---
+++ spatia-room/src/main/java/co/anbora/labs/spatia/builder/SpatiaBuilder.kt
@@ -0,0 +1,80 @@
+package co.anbora.labs.spatia.builder
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteOpenHelper
+import java.util.concurrent.Executor
+
+class SpatiaBuilder<T : RoomDatabase?> (
+    context: Context,
+    klass: Class<T>,
+    name: String
+): SpatiaRoom.Builder<T> {
+
+    private val roomBuilder = Room.databaseBuilder(
+        context.applicationContext,
+        klass,
+        name
+    ).createFromAsset("spatia_db_template.sqlite")
+
+    override fun openHelperFactory(factory: SupportSQLiteOpenHelper.Factory?): SpatiaRoom.Builder<T> {
+        roomBuilder.openHelperFactory(factory)
+        return this
+    }
+
+    override fun addMigrations(vararg migrations: Migration): SpatiaRoom.Builder<T> {
+        roomBuilder.addMigrations(*migrations)
+        return this
+    }
+
+    override fun allowMainThreadQueries(): SpatiaRoom.Builder<T> {
+        roomBuilder.allowMainThreadQueries()
+        return this
+    }
+
+    override fun setJournalMode(journalMode: RoomDatabase.JournalMode): SpatiaRoom.Builder<T> {
+        roomBuilder.setJournalMode(journalMode)
+        return this
+    }
+
+    override fun setQueryExecutor(executor: Executor): SpatiaRoom.Builder<T> {
+        roomBuilder.setQueryExecutor(executor)
+        return this
+    }
+
+    override fun setTransactionExecutor(executor: Executor): SpatiaRoom.Builder<T> {
+        roomBuilder.setTransactionExecutor(executor)
+        return this
+    }
+
+    override fun enableMultiInstanceInvalidation(): SpatiaRoom.Builder<T> {
+        roomBuilder.enableMultiInstanceInvalidation()
+        return this
+    }
+
+    override fun fallbackToDestructiveMigration(): SpatiaRoom.Builder<T> {
+        roomBuilder.fallbackToDestructiveMigration()
+        return this
+    }
+
+    override fun fallbackToDestructiveMigrationOnDowngrade(): SpatiaRoom.Builder<T> {
+        roomBuilder.fallbackToDestructiveMigrationOnDowngrade()
+        return this
+    }
+
+    override fun fallbackToDestructiveMigrationFrom(vararg startVersions: Int): SpatiaRoom.Builder<T> {
+        roomBuilder.fallbackToDestructiveMigrationFrom(*startVersions)
+        return this
+    }
+
+    override fun addCallback(callback: RoomDatabase.Callback): SpatiaRoom.Builder<T> {
+        roomBuilder.addCallback(callback)
+        return this
+    }
+
+    override fun build(): T = roomBuilder.build()
+
+
+}
