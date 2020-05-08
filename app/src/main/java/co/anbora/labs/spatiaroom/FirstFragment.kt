bc0c68491b694dfe2b03803f02ad25f5b4b5efa1
Darwin Algarin <dalgarins@live.com>
2020-05-07 23:23:40 -0500

    Added demo application for spatia lite functions.

--- app/src/main/java/co/anbora/labs/spatiaroom/FirstFragment.kt
+++ app/src/main/java/co/anbora/labs/spatiaroom/FirstFragment.kt
@@ -1,17 +1,24 @@
 package co.anbora.labs.spatiaroom
 
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import co.anbora.labs.spatiaroom.data.AppDatabase
+import co.anbora.labs.spatiaroom.data.model.Post
+import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+
+    private lateinit var appDatabase: AppDatabase
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +31,30 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        appDatabase = AppDatabase.getInstance(requireContext())
+
+        AsyncTask.execute(Runnable {
+
+            val post1 = Post(1, "prueba", "darwin", "spatia", "test.img")
+            val post2 = Post(2, "prueba2", null, "spatia2", "test2.img")
+
+            val listPost = listOf(post1, post2)
+
+            appDatabase.getPostsDao().insertPosts(listPost)
+
+            val spatiaVersion = appDatabase.getPostsDao().getSpatiaVersion()
+            val proj4Version = appDatabase.getPostsDao().getProj4Version()
+            val geosVersion = appDatabase.getPostsDao().getGeosVersion()
+            val makePoliline = appDatabase.getPostsDao().getMakePolyline()
+            val distance = appDatabase.getPostsDao().getDistance()
+            val post = appDatabase.getPostsDao().getAllPostsList()
+
+            spatia_version.text = spatiaVersion
+            proj4_version.text = proj4Version
+            geos_version.text = geosVersion
+            polyline_txt.text = makePoliline
+            distance_txt.text = distance.toString()
+
+        })
     }
 }
---
+++ app/src/main/java/co/anbora/labs/spatiaroom/data/AppDatabase.kt
@@ -0,0 +1,49 @@
+package co.anbora.labs.spatiaroom.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.Room.databaseBuilder
+import androidx.room.RoomDatabase
+import co.anbora.labs.spatia.builder.SpatiaRoom
+import co.anbora.labs.spatia.db.SpatiaHelperFactory
+import co.anbora.labs.spatiaroom.data.dao.PostsDao
+import co.anbora.labs.spatiaroom.data.model.Post
+
+@Database(
+    entities = [Post::class],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    /**
+     * @return [PostsDao] Foodium Posts Data Access Object.
+     */
+    abstract fun getPostsDao(): PostsDao
+
+    companion object {
+        const val DB_NAME = "geo_database"
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                val instance = SpatiaRoom.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    DB_NAME
+                ).build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+
+    }
+}
